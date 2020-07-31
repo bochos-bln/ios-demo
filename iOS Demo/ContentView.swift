6fed45383e159e8ec205f5d7f2933d44cafc09f0
Ringo Müller (iMac05) <ringo.mueller@xailabs.de>
2020-07-31 10:23:28 +0200

    Initial Commit

---
+++ iOS Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//
+// FILENAME
+//
+// Created by Ringo Müller-Gromes on 31.07.20.
+// Copyright © 2020 Ringo Müller-Gromes for "taz" digital newspaper. All rights reserved.
+// 
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
